c3a478abb8f368b76fb67243ec972560734a101a
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-11-26 12:35:39 +0900

    볼링공 고르기: 주어진 입력 조건을 이용해서 재대로 푼 경우

--- algorizmStudy/ChooseBalling.py
+++ algorizmStudy/ChooseBalling.py
@@ -1,22 +1,29 @@
 #볼링공 고르기
 #볼링공의 개수와 공의 최대 볼링공의 무게에 관계없으 리스트 데이터 만으로 해결
-#이방법으로 푸는게 아닌것 같다. 다시 풀어봐야할 문제
+#볼링공의 최대 무개는 10
 
-from itertools import combinations
 
 #볼링공의 개수 N, 공의 최대 볼링공 무게 M
 N, M = map(int, input().split())
 
 #각 볼링공의 무게
 data = list(map(int, input().split()))
-combi = list(combinations(data, 2))
-cnt = 0
 
-for i in combi:
-    if i[0] == i[1]:
-        continue
-    cnt +=1
+arr = [0] * 11
+result = 0
+for i in data:
 
-print(cnt)
+    arr[i] += 1
+
+#1~부터 최대무개(M)까지의 무게에 대해 처리
+for i in range(1, M+1):
+    #무개가 i인 볼링공에 대한 개수 제외  (A가 선택할 수 있는 개수
+    N -=arr[i]
+
+    #B가 선택할 수 있는 개수
+    result += arr[i] * N
+
+
+print(result)
 
 
