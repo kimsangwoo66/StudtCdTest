3b882e8075b174df5910c04452e0eb7105ecd6aa
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-11-26 15:18:51 +0900

    dfs 알고리즘 , 주석 추가

--- algorizmStudy/dfs.py
+++ algorizmStudy/dfs.py
@@ -21,8 +21,12 @@ def dfs(graph, v, visited):
     #해당 노드 방문처리
     visited[v] = True
     print(v, end=' ')
+
+    #현재 노드와 인접한 노드 탐색
     for i in graph[v]:
+        #현재 노드와 인접한 노드를 방문하지 않은 상태라면
         if not visited[i]:
+            #재귀함수 호출
             dfs(graph, i, visited)
 
 #노드 번호가 작은 노드부터 탐색 시작
