40a0c3c922f5716a105a9b10445e19cc434a62fa
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-11-26 15:15:28 +0900

    dfs 알고리즘

---
+++ algorizmStudy/dfs.py
@@ -0,0 +1,31 @@
+#dfs 깊이 우선 탐색
+
+#각 노드가 연결된 정보 표현 , 무방향 그래프
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+#현재 노드를 방문 처리
+visited = [False] * 9
+
+
+def dfs(graph, v, visited):
+    #해당 노드 방문처리
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+#노드 번호가 작은 노드부터 탐색 시작
+result = dfs(graph, 1, visited)
+
+print(result)
