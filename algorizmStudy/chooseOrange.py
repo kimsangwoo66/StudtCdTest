b27e9782953b7f6869dc6c43408de981f9d33523
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2022-12-02 02:08:20 +0900

    귤 고르기 : 다시풀기 완전탐색

---
+++ algorizmStudy/chooseOrange.py
@@ -0,0 +1,21 @@
+#딕녀서리로 갯수 카운트
+
+#수확한 귤의 크기 중 개수가 가장 적은 귤의 개수를 하나씩 제거
+
+#
+
+def solution(k, tangerine):
+
+    Odict = {}
+
+    for i in tangerine:
+        Odict[i] +=1
+    answer = 0
+    return answer
+
+
+
+
+k = 6
+tangerine = [1, 3, 2, 5, 4, 5, 2, 3]
+result = solution(k, tangerine)
